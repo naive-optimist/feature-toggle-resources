72030ae07f3e5310693c03eca83b414423169486
MICHAEL BOURGEOIS <Michael.Bourgeois@LibertyMutual.com>
2021-09-07 09:36:48 -0400

    Init commit

--- lib/toggles-stack.ts
+++ lib/toggles-stack.ts
@@ -1,18 +1,54 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
-import * as cdk from '@aws-cdk/core';
+import {App, CfnOutput, Stack, StackProps, RemovalPolicy} from '@aws-cdk/core';
+import { Runtime, Function, Code, DockerImageFunction, DockerImageCode } from '@aws-cdk/aws-lambda';
+import { Table, AttributeType, BillingMode } from '@aws-cdk/aws-dynamodb';
+import apigw = require('@aws-cdk/aws-apigatewayv2');
+import integrations = require('@aws-cdk/aws-apigatewayv2-integrations');
+import * as path from 'path'
 
-export class TogglesStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+export class TogglesStack extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'TogglesQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
+    //DynamoDB Table
+    const toggleTable = new Table(this, 'Toggles', {
+      tableName: 'toggles-table',
+      partitionKey: { name: 'feature', type: AttributeType.STRING },
+      serverSideEncryption: true, 
+      timeToLiveAttribute: 'ttl',
+      billingMode: BillingMode.PAY_PER_REQUEST, 
+      removalPolicy: RemovalPolicy.DESTROY,
+    })
 
-    const topic = new sns.Topic(this, 'TogglesTopic');
+    // defines AWS Lambda
+    const getToggleLambda = new Function(this, 'GetToggleHandler', {
+      runtime: Runtime.NODEJS_12_X,      
+      code: Code.fromAsset('./lambdas'),  
+      handler: './lib/app.getToggle',                
+      environment: {
+        TOGGLE_TABLE_NAME: toggleTable.tableName
+      }
+    })
+    toggleTable.grantReadWriteData(getToggleLambda);
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const setToggleLambda = new Function(this, 'SetToggleHandler', {
+      runtime: Runtime.NODEJS_12_X,      
+      code: Code.fromAsset('./lambdas'),  
+      handler: './lib/app.setToggle',                
+      environment: {
+        TOGGLE_TABLE_NAME: toggleTable.tableName
+      }
+    })
+    toggleTable.grantReadWriteData(setToggleLambda);
+
+    // defines an API Gateway Http API resource backed by our "dynamoLambda" function.
+    let api = new apigw.HttpApi(this, 'Toggle-API', {
+      defaultIntegration: new integrations.LambdaProxyIntegration({
+        handler: getToggleLambda
+      })
+    })
+
+   new CfnOutput(this, 'HTTP API Url', {
+     value: api.url ?? 'Something went wrong with the deploy'
+   })
   }
 }
